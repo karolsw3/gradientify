5d2b8662d4c630d662fb2a412692db8ee4e567b3
Karol Świerczek <karol.sw3@gmail.com>
2018-02-23 00:46:10 +0100

    Add opacity control
    It's now possible to set opacity of gradients, which is especially useful for creating overlaying gradients.

--- src/gradientify.js
+++ src/gradientify.js
@@ -1,29 +1,52 @@
 class Gradientify {
-	constructor() {
 
+	constructor(){
+		this.background = []
+		this.errorMessage = `Gradientify:`
+		this.changeBackground = this.changeBackground.bind(this)
 	}
 
-	create(element, gradients, options) {
-
-		var background = []
+	changeBackground(element, gradients, options) {
+		if(typeof gradients !== `object`){
+			throw `${this.errorMessage} Invalid gradients`
+			return
+		}
 
 		gradients.map((gradient, i) => {
+
+			if(gradient.opacity > 0){
+				gradient.opacity = Math.round(gradient.opacity*255)
+				gradient.colors = gradient.colors.replace(/\s/g, '').split(`,`).map(c => {
+					var hex = gradient.opacity.toString(16)
+					console.log(hex)
+					return c+(hex.length === 1 ? "0" + hex : hex)
+				}).join(`,`)
+
+				if(options.debug){ 
+					console.log(gradient.colors)
+				}
+			}
+
 			switch (gradient.type) {
 				case `linear`:
-					background.push(`linear-gradient(${gradient.angle}, ${gradient.colors})`)
+					this.background.push(`linear-gradient(${gradient.angle}, ${gradient.colors})`)
 					break
 				case `radial`:
 					if (gradient.shape === undefined) gradient.shape = `ellipse`
-					background.push(`radial-gradient(${gradient.shape}, ${gradient.colors})`)
+					this.background.push(`radial-gradient(${gradient.shape}, ${gradient.colors})`)
 					break
 				default:
-					throw `Gradientify: Invalid gradient type "${gradient.type}"`
+					throw `${this.errorMessage} Invalid gradient type "${gradient.type}"`
 					break
 			}
+
 		})
 
-		if(options.debug) console.log(background.toString())
-		element.style.backgroundImage = background.toString()+`;`
+		if(options.debug){ 
+			console.log(this.background.toString())
+		}
+
+		element.style.backgroundImage = this.background.toString()
 	}
 }
 
@@ -31,14 +54,16 @@ class Gradientify {
 
 var gradientify = new Gradientify()
 
-gradientify.create(document.body, [{
+gradientify.changeBackground(document.body, [{
 	type: `linear`,
 	angle: `60deg`,
-	colors: `red, blue, black`
+	colors: `#11ff33, #aa3fff`,
+	opacity: .5
 },{
 	type: `radial`,
 	angle: `94deg`,
-	colors: `orange, white`
+	colors: `#1133ff, #22ffff`,
+	opacity: 1
 }], {
 	debug: true
 })
