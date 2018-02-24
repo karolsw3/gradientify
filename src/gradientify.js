fbeba943fbc00b6e647570a1ae0be20ec4de4c0a
Karol Świerczek <karol.sw3@gmail.com>
2018-02-24 23:12:33 +0100

    Remove redundant code

--- src/gradientify.js
+++ src/gradientify.js
@@ -1,14 +1,12 @@
 class Gradientify {
 
 	constructor(input){
-
 		this.gradients = []
 		this.mainElement = input.element
 		this.mainGradientIndex = 0
 		this.animate = this.animate.bind(this)
 
 		input.gradients.map(gradient => {
-
 			let newElement = document.createElement("div")
 
 			Object.assign(newElement.style, {
@@ -18,6 +16,7 @@ class Gradientify {
 				height: `100%`,
 				position: `absolute`,
 				transitionTimingFunction: `linear`,
+				transitionDuration: `${input.interval/1000}s`,
 				top: `0`,
 				left: `0`,
 				zIndex: `-999`
@@ -27,27 +26,19 @@ class Gradientify {
 			this.mainElement.append(newElement)
 		})
 
-		this.gradients.map(gradient => {
-			gradient.style.transitionDuration = `${input.interval/1000}s`
-		})
-
 		this.animate()
 
 		setInterval(()=>{
 			this.animate()
 		}, input.interval+input.delay+50)
-
 	}
 
 	animate(){
-
 		this.gradients.map((gradient, gradientIndex) => {
 			if(gradientIndex === this.mainGradientIndex) gradient.style.opacity = 1
 			else gradient.style.opacity = 0
 		})
 
 		this.mainGradientIndex = (++this.mainGradientIndex % this.gradients.length)
-		
 	}
-
 }
