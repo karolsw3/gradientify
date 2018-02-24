c050549d72a164490481cf27f897f3893a23f38e
Karol Świerczek <karol.sw3@gmail.com>
2018-02-24 23:08:19 +0100

    Reduce animation initalisation to constructor only

--- src/gradientify.js
+++ src/gradientify.js
@@ -1,10 +1,10 @@
 class Gradientify {
 
 	constructor(input){
+
 		this.gradients = []
 		this.mainElement = input.element
 		this.mainGradientIndex = 0
-
 		this.animate = this.animate.bind(this)
 
 		input.gradients.map(gradient => {
@@ -25,24 +25,29 @@ class Gradientify {
 
 			this.gradients.push(newElement)
 			this.mainElement.append(newElement)
-
 		})
-	}
-
-	animate(input) {
 
 		this.gradients.map(gradient => {
 			gradient.style.transitionDuration = `${input.interval/1000}s`
 		})
 
+		this.animate()
+
 		setInterval(()=>{
-			this.gradients.map((gradient, gradientIndex) => {
-				if(gradientIndex === this.mainGradientIndex) gradient.style.opacity = 1
-				else gradient.style.opacity = 0
-			})
-			this.mainGradientIndex = (++this.mainGradientIndex % this.gradients.length)
+			this.animate()
 		}, input.interval+input.delay+50)
 
+	}
+
+	animate(){
+
+		this.gradients.map((gradient, gradientIndex) => {
+			if(gradientIndex === this.mainGradientIndex) gradient.style.opacity = 1
+			else gradient.style.opacity = 0
+		})
+
+		this.mainGradientIndex = (++this.mainGradientIndex % this.gradients.length)
+		
 	}
 
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ var gradientify = new Gradientify({
 		{gradient: `linear-gradient(60deg, rgba(255, 0, 0), rgba(0, 0, 255))`, opacity: 1},
 		{gradient: `linear-gradient(10deg, rgba(25, 123, 23), rgba(255, 0, 155))`, opacity: 0},
 		{gradient: `radial-gradient(rgba(25, 123, 223), rgba(5, 133, 5))`, opacity: 0}
-	]
-})
-
-gradientify.animate({
+	],
 	delay: 0,
 	interval: 2300
 })
