a4d16602d112879b88c0cfef582a3d32e27db30d
Karol Świerczek <karol.sw3@gmail.com>
2018-02-26 16:26:12 +0100

    Change style guide of the code to Standard.js

--- src/gradientify.js
+++ src/gradientify.js
@@ -1,51 +1,50 @@
 class Gradientify {
+  constructor (input) {
+    this.gradients = []
+    this.mainElement = input.element
+    this.mainGradientIndex = 0
+    this.animate = this.animate.bind(this)
+    this.init = this.init.bind(this)
 
-	constructor(input){
-		this.gradients = []
-		this.mainElement = input.element
-		this.mainGradientIndex = 0
-		this.animate = this.animate.bind(this)
-		this.init = this.init.bind(this)
+    this.init(input)
+  }
 
-		this.init(input)
-	}
+  init (input) {
+    input.gradients.map((gradient, gradientIndex) => {
+      let newElement = document.createElement('div')
 
-	init(input){
-		input.gradients.map((gradient, gradientIndex) => {
-			let newElement = document.createElement("div")
+      if (input.fixed) this.mainElement.style.overflow = `hidden`
 
-			if(input.fixed) this.mainElement.style.overflow = `hidden`
+      Object.assign(newElement.style, {
+        backgroundImage: gradient,
+        opacity: (gradientIndex === this.mainGradientIndex) ? 1 : 0,
+        width: `100%`,
+        height: `100%`,
+        transitionTimingFunction: `linear`,
+        transitionDuration: `${input.interval / 1000}s`,
+        position: input.fixed ? `fixed` : `absolute`,
+        top: `0`,
+        left: `0`,
+        zIndex: `-999`
+      })
 
-			Object.assign(newElement.style, {
-				backgroundImage: gradient,
-				opacity: (gradientIndex === this.mainGradientIndex) ? 1:0,
-				width: `100%`,
-				height: `100%`,
-				transitionTimingFunction: `linear`,
-				transitionDuration: `${input.interval/1000}s`,
-				position: input.fixed ? `fixed`:`absolute`,
-				top: `0`,
-				left: `0`,
-				zIndex: `-999`
-			})
+      this.gradients.push(newElement)
+      this.mainElement.append(newElement)
+    })
 
-			this.gradients.push(newElement)
-			this.mainElement.append(newElement)
-		})
+    this.animate()
 
-		this.animate()
+    setInterval(() => {
+      this.animate()
+    }, input.interval + input.delay + 40)
+  }
 
-		setInterval(()=>{
-			this.animate()
-		}, input.interval+input.delay+40)
-	}
+  animate () {
+    this.gradients.map((gradient, gradientIndex) => {
+      if (gradientIndex === this.mainGradientIndex) gradient.style.opacity = 1
+      else gradient.style.opacity = 0
+    })
 
-	animate(){
-		this.gradients.map((gradient, gradientIndex) => {
-			if(gradientIndex === this.mainGradientIndex) gradient.style.opacity = 1
-			else gradient.style.opacity = 0
-		})
-
-		this.mainGradientIndex = (++this.mainGradientIndex % this.gradients.length)
-	}
+    this.mainGradientIndex = (++this.mainGradientIndex % this.gradients.length)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
-new Gradientify({
-	element: document.getElementById(`header`),
-	gradients: [
-		`linear-gradient(60deg, rgba(255, 0, 0), rgba(0, 0, 255))`,
-		`linear-gradient(10deg, rgba(25, 123, 23), rgba(255, 0, 155))`,
-		`radial-gradient(rgba(25, 123, 223), rgba(5, 133, 5))`,
-		`linear-gradient(130deg, rgba(25, 23, 223), rgba(25, 89, 5), rgba(15, 189, 176))`,
-	],
-	fixed: false,
-	delay: 0,
-	interval: 1300
+var gradient = new Gradientify({
+  element: document.getElementById(`header`),
+  gradients: [
+    `linear-gradient(60deg, rgba(255, 0, 0), rgba(0, 0, 255))`,
+    `linear-gradient(10deg, rgba(25, 123, 23), rgba(255, 0, 155))`,
+    `radial-gradient(rgba(25, 123, 223), rgba(5, 133, 5))`,
+    `linear-gradient(130deg, rgba(25, 23, 223), rgba(25, 89, 5), rgba(15, 189, 176))`
+  ],
+  fixed: false,
+  delay: 0,
+  interval: 1300
 })
