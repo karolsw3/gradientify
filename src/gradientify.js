daba9a83e737fccb05beaf92caa6d1afb4677021
Karol Świerczek <karol.sw3@gmail.com>
2018-02-26 17:23:32 +0100

    Split the code into smaller functions to make it cleaner

--- src/gradientify.js
+++ src/gradientify.js
@@ -3,48 +3,64 @@ class Gradientify {
     this.gradients = []
     this.mainElement = input.element
     this.mainGradientIndex = 0
-    this.animate = this.animate.bind(this)
+
     this.init = this.init.bind(this)
+    this.createGradient = this.createGradient.bind(this)
+    this.initialiseInterval = this.initialiseInterval.bind(this)
+    this.makeNewGradientVisible = this.makeNewGradientVisible.bind(this)
 
     this.init(input)
   }
 
   init (input) {
     input.gradients.map((gradient, gradientIndex) => {
-      let newElement = document.createElement('div')
-
+      let newGradient = this.createGradient({
+        backgroundImage: gradient,
+        index: gradientIndex,
+        position: input.fixed,
+        transitionDuration: input.interval
+      })
       if (input.fixed) this.mainElement.style.overflow = `hidden`
 
-      Object.assign(newElement.style, {
-        backgroundImage: gradient,
-        opacity: (gradientIndex === this.mainGradientIndex) ? 1 : 0,
-        width: `100%`,
-        height: `100%`,
-        transitionTimingFunction: `linear`,
-        transitionDuration: `${input.interval / 1000}s`,
-        position: input.fixed ? `fixed` : `absolute`,
-        top: `0`,
-        left: `0`,
-        zIndex: `-999`
-      })
-
-      this.gradients.push(newElement)
-      this.mainElement.append(newElement)
+      this.gradients.push(newGradient)
+      this.mainElement.append(newGradient)
     })
 
-    this.animate()
-
-    setInterval(() => {
-      this.animate()
-    }, input.interval + input.delay + 40)
+    this.initialiseInterval({
+      interval: input.interval,
+      delay: input.delay
+    })
   }
 
-  animate () {
+  createGradient (gradientConfig) {
+    let newElement = document.createElement('div')
+    Object.assign(newElement.style, {
+      backgroundImage: gradientConfig.backgroundImage,
+      opacity: (gradientConfig.index === this.mainGradientIndex) ? 1 : 0,
+      width: `100%`,
+      height: `100%`,
+      transitionTimingFunction: `linear`,
+      transitionDuration: `${gradientConfig.transitionDuration / 1000}s`,
+      position: gradientConfig.fixed ? `fixed` : `absolute`,
+      top: `0`,
+      left: `0`,
+      zIndex: `-999`
+    })
+    return newElement
+  }
+
+  initialiseInterval (intervalConfig) {
+    this.makeNewGradientVisible()
+    setInterval(() => {
+      this.makeNewGradientVisible()
+    }, intervalConfig.interval + intervalConfig.delay + 40)
+  }
+
+  makeNewGradientVisible () {
     this.gradients.map((gradient, gradientIndex) => {
       if (gradientIndex === this.mainGradientIndex) gradient.style.opacity = 1
       else gradient.style.opacity = 0
     })
-
     this.mainGradientIndex = (++this.mainGradientIndex % this.gradients.length)
   }
 }
