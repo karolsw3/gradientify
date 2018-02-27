15bf3e5c692a908dc6fd2762a1d114d65d07ba06
Karol Świerczek <karol.sw3@gmail.com>
2018-02-27 07:55:42 +0100

    Fix gulpfile indentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-const debug = require('gulp-debug');
-var stylus = require('gulp-stylus');
-var uglify = require('gulp-uglify-es').default;
-var pump = require('pump');
+var gulp = require('gulp')
+var pug = require('gulp-pug')
+const debug = require('gulp-debug')
+var stylus = require('gulp-stylus')
+var uglify = require('gulp-uglify-es').default
+var pump = require('pump')
 
-gulp.task('html', function(){
-	return gulp.src('src/*.pug')
-		.pipe(debug({title: 'Html:'}))
+gulp.task('html', function () {
+  return gulp.src('src/*.pug')
+    .pipe(debug({title: 'Html:'}))
     .pipe(pug())
     .pipe(gulp.dest('build'))
-});
+})
 
-gulp.task('css', function(){
-	return gulp.src('src/*.styl')
-		.pipe(debug({title: 'Css:'}))
+gulp.task('css', function () {
+  return gulp.src('src/*.styl')
+    .pipe(debug({title: 'Css:'}))
     .pipe(stylus())
     .pipe(gulp.dest('build'))
-});
+})
 
-gulp.task('compress', function (cb){
+gulp.task('compress', function (cb) {
   pump([
-        gulp.src('src/*.js'),
-        uglify(),
-        gulp.dest('build')
-    ],
-    cb
-  );
-});
+    gulp.src('src/*.js'),
+    uglify(),
+    gulp.dest('build')
+  ],
+  cb
+  )
+})
 
-gulp.task('default', [ 'html', 'css', 'compress' ]);
+gulp.task('default', [ 'html', 'css', 'compress' ])
