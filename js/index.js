4d88a0291014cf43a805357b4fe4e6c27bfd2162
Karol Świerczek <karol.sw3@gmail.com>
2018-02-28 18:19:06 +0100

    Change leading gradients colors to more marvelous

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-var gradient=new Gradientify({element:document.getElementById("header"),gradients:["linear-gradient(60deg, rgba(255, 0, 0), rgba(0, 0, 255))","linear-gradient(10deg, rgba(25, 123, 23), rgba(255, 0, 155))","radial-gradient(rgba(25, 123, 223), rgba(5, 133, 5))","linear-gradient(130deg, rgba(235, 23, 223), rgba(25, 89, 135), rgba(15, 189, 176))"],fixed:!1,delay:0,interval:2600});
+var g = new Gradientify({
+  element: document.getElementById('header'),
+  gradients: ['linear-gradient(60deg, rgb(255, 91, 91), rgb(0, 0, 255))',
+    'linear-gradient(10deg, rgb(180, 132, 203), rgb(255, 0, 155))',
+    'radial-gradient(at center center, rgb(75, 35, 148), rgb(0, 71, 57))',
+    'linear-gradient(130deg, rgb(235, 23, 223), rgb(25, 89, 135), rgb(15, 189, 176))'],
+  fixed: !1,
+  delay: 0,
+  interval: 2600
+})
