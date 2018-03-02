d36be664f7b410a7eb9beb11da801e2199e6d0d4
Karol Świerczek <karol.sw3@gmail.com>
2018-03-02 17:25:21 +0100

    Add loading presets by getPreset function

--- src/gradientify.js
+++ src/gradientify.js
@@ -1,5 +1,7 @@
 class Gradientify {
   constructor (input) {
+    this.input = input
+    this.interval = null
     this.gradients = []
     this.mainElement = input.element
     this.mainGradientIndex = 0
@@ -10,26 +12,28 @@ class Gradientify {
     this.makeNewGradientVisible = this.makeNewGradientVisible.bind(this)
     this.loadJSON = this.loadJSON.bind(this)
 
-    this.init(input)
+    this.init()
   }
 
-  init (input) {
-    input.gradients.map((gradient, gradientIndex) => {
+  init () {
+    this.gradients = []
+    this.input.gradients.map((gradient, gradientIndex) => {
       let newGradient = this.createGradientElement({
         backgroundImage: gradient,
         index: gradientIndex,
-        position: input.fixed,
-        transitionDuration: input.interval
+        position: this.input.fixed,
+        transitionDuration: this.input.interval
       })
-      if (input.fixed) this.mainElement.style.overflow = `hidden`
+      if (this.input.fixed) this.mainElement.style.overflow = `hidden`
 
       this.gradients.push(newGradient)
       this.mainElement.append(newGradient)
     })
 
+    clearInterval(this.interval)
     this.initialiseInterval({
-      interval: input.interval,
-      delay: input.delay
+      interval: this.input.interval,
+      delay: this.input.delay
     })
   }
 
@@ -50,7 +54,9 @@ class Gradientify {
 
   getPreset (presetHash) {
     this.loadJSON((data) => {
-      this.gradients = data[presetHash].gradients
+      this.input.gradients = data[presetHash].gradients
+      console.log(data[presetHash].gradients)
+      this.init()
     })
   }
 
@@ -60,7 +66,7 @@ class Gradientify {
     xobj.open('GET', './src/presets.json', true)
     xobj.onreadystatechange = function () {
       if (xobj.readyState === 4 && xobj.status === 200) {
-        callback(xobj.responseText)
+        callback(JSON.parse(xobj.responseText))
       }
     }
     xobj.send(null)
@@ -68,7 +74,7 @@ class Gradientify {
 
   initialiseInterval (intervalConfig) {
     this.makeNewGradientVisible()
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.makeNewGradientVisible()
     }, intervalConfig.interval + intervalConfig.delay + 40)
   }
