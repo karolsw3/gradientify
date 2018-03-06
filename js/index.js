b7462c168695f6167b37a9d96e0ea38950e83c47
Karol Świerczek <karol.sw3@gmail.com>
2018-03-06 20:51:58 +0100

    Include Vue and use it to load presets

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-var g = new Gradientify({
+
+var background = {...gradientify}
+background.init({
   element: document.body,
   gradients: ['linear-gradient(60deg, rgb(255, 91, 91), rgb(0, 0, 255))',
     'linear-gradient(10deg, rgb(180, 132, 203), rgb(255, 0, 155))',
@@ -9,10 +11,22 @@ var g = new Gradientify({
   interval: 2600
 })
 
-var a = new Gradientify({
-  element: document.getElementById('f4a4dF'),
-  gradients: 'f4a4dF',
-  fixed: !1,
-  delay: 0,
-  interval: 2600
+var app = new Vue({
+  el: '#presets',
+  data: {
+    gradients: [],
+    presets: ''
+  },
+  updated: function () {
+    this.gradients.push({...gradientify})
+    this.gradients[0].loadPreset({
+      element: document.getElementById(`f4a4dF`),
+      hash: `f4a4dF`
+    })
+  }
+})
+
+gradientify.loadPresetsJSON('https://raw.githubusercontent.com/karolsw2/gradientify.js/master/build/presets.json', () => {
+  app.presets = gradientify.presets
+  console.log(`Presets loaded!`)
 })
