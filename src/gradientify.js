5fa5f432b9dda773794ba7cae051eb55ec020183
Karol Świerczek <karol.sw3@gmail.com>
2018-03-07 13:36:16 +0100

    Change gradients var name to gradientsCSS

--- src/gradientify.js
+++ src/gradientify.js
@@ -1,6 +1,6 @@
 function Gradientify() {
   let interval = 3000
-  let gradients = []
+  let gradientsCSS = []
   let mainElement = document.body
   let mainGradientIndex = 0
   let gradientElements = []
@@ -9,7 +9,7 @@ function Gradientify() {
 
   function init (input) {
     mainElement = input.element
-    gradients = input.gradients
+    gradientsCSS = input.gradients
 
     appendGradientsOnMainElement({
       interval: input.interval
@@ -24,7 +24,7 @@ function Gradientify() {
   function loadPreset (preset) {
     mainElement = preset.element
 
-    gradients = presets[preset.hash].gradients
+    gradientsCSS = presets[preset.hash].gradients
     appendGradientsOnMainElement({
       interval: presets[preset.hash].interval
     })
@@ -36,7 +36,7 @@ function Gradientify() {
   }
 
   function appendGradientsOnMainElement (input) {
-    gradients.map((gradient, gradientIndex) => {
+    gradientsCSS.map((gradient, gradientIndex) => {
       let newGradient = createGradientElement({
         backgroundImage: gradient,
         index: gradientIndex,
@@ -69,7 +69,7 @@ function Gradientify() {
     xobj.onreadystatechange = function () {
       if (xobj.readyState === 4 && xobj.status === 200) {
         presets = JSON.parse(xobj.responseText)
-        callback()
+        callback(presets)
       }
     }
     xobj.send(null)
