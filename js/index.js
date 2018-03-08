f57f05b681ee8d5596b051ce6ef30be925fc64b5
Karol Świerczek <karol.sw3@gmail.com>
2018-03-08 21:48:17 +0100

    Make displaying presets work

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
 
-var background = {...gradientify}
-background.init({
-  element: document.body,
-  gradients: ['linear-gradient(60deg, rgb(255, 91, 91), rgb(0, 0, 255))',
-    'linear-gradient(10deg, rgb(180, 132, 203), rgb(255, 0, 155))',
-    'radial-gradient(at center center, rgb(75, 35, 148), rgb(0, 71, 57))',
-    'linear-gradient(130deg, rgb(235, 23, 223), rgb(25, 89, 135), rgb(15, 189, 176))'],
-  fixed: !1,
-  delay: 0,
-  interval: 2600
-})
+var gf = Gradientify()
+gf.gradientifize(document.body, [
+  'linear-gradient(60deg, rgb(255, 91, 91), rgb(0, 0, 255))',
+  'linear-gradient(10deg, rgb(180, 132, 203), rgb(255, 0, 155))',
+  'radial-gradient(at center center, rgb(75, 35, 148), rgb(0, 71, 57))',
+  'linear-gradient(130deg, rgb(235, 23, 223), rgb(25, 89, 135), rgb(15, 189, 176))'
+], 2600)
 
 var app = new Vue({
   el: '#presets',
   data: {
-    gradients: [],
-    presets: ''
-  },
-  updated: function () {
-    this.gradients.push({...gradientify})
-    this.gradients[0].loadPreset({
-      element: document.getElementById(`f4a4dF`),
-      hash: `f4a4dF`
-    })
+    presets: [{}, {}, {}, {}, {}, {}, {}, {}, {}]
   }
 })
 
-gradientify.loadPresetsJSON('https://raw.githubusercontent.com/karolsw2/gradientify.js/master/build/presets.json', () => {
-  app.presets = gradientify.presets
-  console.log(`Presets loaded!`)
-})
+let xobj = new XMLHttpRequest()
+xobj.overrideMimeType('application/json')
+xobj.open('GET', 'https://rawgit.com/karolsw2/gradientify.js/optimisation__change-design-pattern/build/presets.json', true)
+xobj.onreadystatechange = function () {
+  if (xobj.readyState === 4 && xobj.status === 200) {
+    app.presets = JSON.parse(xobj.responseText)
+    Vue.nextTick().then(function () {
+      app.presets.map(preset => {
+        console.log(preset)
+        gf.gradientifize(document.getElementById(preset.id), preset.id, 2000)
+      })
+    })
+  }
+}
+xobj.send(null)
