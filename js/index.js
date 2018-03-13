94835fd4f00be5a2bc591107a5cad75fecdab188
Karol Świerczek <karol.sw3@gmail.com>
2018-03-13 16:54:31 +0100

    Add function to shuffle displayed gradients

--- js/index.js
+++ js/index.js
@@ -1,27 +1,37 @@
-'use strict';
+'use strict'
 
-var gf = Gradientify();
+var gf = Gradientify()
 
-gf.create(document.body, ['linear-gradient(60deg, rgb(255, 91, 91), rgb(0, 0, 255))', 'linear-gradient(10deg, rgb(180, 132, 203), rgb(255, 0, 155))', 'radial-gradient(at center center, rgb(75, 35, 148), rgb(0, 71, 57))', 'linear-gradient(130deg, rgb(235, 23, 223), rgb(25, 89, 135), rgb(15, 189, 176))'], 2600);
+gf.create(document.body, ['linear-gradient(60deg, rgb(255, 91, 91), rgb(0, 0, 255))', 'linear-gradient(10deg, rgb(180, 132, 203), rgb(255, 0, 155))', 'radial-gradient(at center center, rgb(75, 35, 148), rgb(0, 71, 57))', 'linear-gradient(130deg, rgb(235, 23, 223), rgb(25, 89, 135), rgb(15, 189, 176))'], 2600)
 
 var app = new Vue({
   el: '#presets',
   data: {
     presets: []
   }
-});
+})
 
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType('application/json');
-xobj.open('GET', 'https://rawgit.com/karolsw2/gradientify.js/master/build/presets.json', true);
+function shuffleArray (array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
+
+var xobj = new XMLHttpRequest()
+xobj.overrideMimeType('application/json')
+xobj.open('GET', 'https://rawgit.com/karolsw2/gradientify.js/master/build/presets.json', true)
 xobj.onreadystatechange = function () {
   if (xobj.readyState === 4 && xobj.status === 200) {
-    app.presets = JSON.parse(xobj.responseText);
+    app.presets = shuffleArray(JSON.parse(xobj.responseText))
     Vue.nextTick().then(function () {
       app.presets.map(function (preset) {
-        gf.load(document.getElementById(preset.hash), preset.hash, 2000);
-      });
-    });
+        gf.load(document.getElementById(preset.hash), preset.hash, 2000)
+      })
+    })
   }
-};
-xobj.send(null);
+}
+xobj.send(null)
