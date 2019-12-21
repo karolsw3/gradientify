50d0e3608f18b91871d2f0848f47997829e24cb6
Charles Spruce <karol.sw3@gmail.com>
2019-12-21 17:57:37 +0100

    fix: opacity of elements

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,13 @@ export default class Gradientify {
   }
 
   private createGradientElements () {
-    this.gradientElements = this.gradients.map(gradient => {
+    this.gradientElements = this.gradients.map((gradient, index) => {
       let element = document.createElement('div')
       Object.assign(element.style, {
         position: 'absolute',
         height: '100%',
         width: '100%',
+        opacity: index === 0 ? 1:0,
         top: 0,
         left: 0,
         transitionTimingFunction: 'linear',
